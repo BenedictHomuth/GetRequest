574a2e1a3c8bbf6f2a8411fbb10c37d8a78c4ba4
BenedictHomuth <benedict.homuth@gmail.com>
2019-12-20 17:11:26 +0100

    Extended the UI, so that we can not only read data from a database, but also send data back to it!
    Todo: Current App -> DB connection is running on Main-Thread!
          This creates a Exception
    
          fix?: - find synchronous implementation of a connection
                - implement everything in an Asynchronous Task!

--- app/src/main/java/com/homuth/getrequest/StartActivity.java
+++ app/src/main/java/com/homuth/getrequest/StartActivity.java
@@ -4,10 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class StartActivity extends AppCompatActivity {
+import java.io.IOException;
 
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class StartActivity extends AppCompatActivity {
+    private static final String TAG = "Start Activity ->";
+    private Button btn_sendData;
+    private TextView tv_serverResonse;
+    private TextView tv_messageFromServer;
+    private TextView tv_sendData;
+    private TextView tv_OnResponseServerQuery;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,9 +31,67 @@ public class StartActivity extends AppCompatActivity {
         Intent intent = getIntent();
 
         String getIT = intent.getStringExtra("intent_data");
+        initUI();
+        tv_messageFromServer.setText(getIT);
 
-        TextView tv = findViewById(R.id.int_Data);
+        btn_sendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeDataToDB();
+            }
+        });
+    }
 
-        tv.setText(getIT);
+    public void initUI(){
+        btn_sendData = findViewById(R.id.btn_sendData);
+        tv_serverResonse = findViewById(R.id.tv_responseFromServer);
+        tv_messageFromServer = findViewById(R.id.tv_messageFromServer);
+        tv_sendData = findViewById(R.id.tv_sendDataToDB);
+        tv_OnResponseServerQuery = findViewById(R.id.tv_OnResponseDataSend);
+    }
+
+    public void writeDataToDB(){
+
+        String httpURL = "http://10.0.2.2/droneapp/api/insertData.php";
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("name", "Mayer")
+                .add("vorname", "Hans")
+                .add("telefon", "5555")
+                .build();
+        final Request request = new Request.Builder()
+                .url(httpURL)
+                .post(formBody)
+                .build();
+
+
+        try {
+            //TODO
+            /*
+            There is an NetworkOnMainThreadException because of obviously it runs on the main thread.
+            Network stuff should never run on the main thread!
+            Try again to implement Asynchronous task or find a synchronous approch to send data to a
+            server.
+
+             */
+
+            final Response response = client.newCall(request).execute();
+
+            Log.d(TAG,"I'm here!");
+
+           StartActivity.this.runOnUiThread(new Runnable() {
+               @Override
+               public void run() {
+                   tv_OnResponseServerQuery.setText(response.body().toString());
+               }
+           });
+        } catch (IOException e) {
+
+            tv_OnResponseServerQuery.setText("That didn't work");
+            //Thread.sleep(5000);
+            e.printStackTrace();
+        }
     }
 }
